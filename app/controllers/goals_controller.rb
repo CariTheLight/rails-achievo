812769b4c4fbb5c23a1e4623349f65f9a9f36a15
Carine Rugorirwera <carinerugorirwera@gmail.com>
2023-09-27 00:11:29 +0200

    Updated routes and controller actions for goals and tasks

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,22 +1,66 @@
 class GoalsController < ApplicationController
-  def index
+  def show
+    @goal = Goal.find(params[:id])
   end
 
-  def show
+  def generate_goal
+    # Use an AI model or service to generate a goal prompt
+    goal_description = generate_goal_description
+
+    # Create a new goal with the generated description
+    @goal = Goal.new(description: goal_description)
+
+    if @goal.save
+      redirect_to @goal, notice: 'New goal generated successfully!'
+    else
+      flash.now[:alert] = 'Error generating the goal.'
+      render :new
+    end
   end
+
+  private
+
+  # Example method to generate a goal description using an AI service
+  def generate_goal_description
+    # You can call an AI service here to generate a goal description
+    # For instance, using an API request to a language model
+    # Replace this with the actual logic to generate a goal description
+    # You might use an external service or your own AI model integration
+    # For simplicity, we'll use a placeholder description here
+    "Learn a new language and become conversational."
+  end
+end
 
   def new
+    @goal = Goal.new
   end
 
   def create
+    @goal = Goal.new(goal_params)
+    @goal.user_id = current_user.id
+
+    if @goal.save!
+      redirect_to goal_path, notice: "Goal was successfully created!"
+    else
+      render :new, alert: "Please try again"
+
   end
 
   def edit
+    @goal = Goal.find(params[:id])
   end
 
   def update
+    @goal = Goal.find(params[:id])
+    if @goal.update(goal_params)
+      redirect_to @goal, notice: "Goal was succesfully updated."
+    else
+      render :edit
   end
 
   def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+    redirect_to goals_url, notice: "Goal was succesfully deleted"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,20 @@
 Rails.application.routes.draw do
-  get 'journals/index'
-  get 'journals/show'
-  get 'journals/new'
-  get 'journals/create'
-  get 'journals/edit'
-  get 'journals/update'
-  get 'journals/destroy'
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/create'
-  get 'tasks/edit'
-  get 'tasks/update'
-  get 'tasks/destroy'
-  get 'goals/index'
-  get 'goals/show'
-  get 'goals/new'
-  get 'goals/create'
-  get 'goals/edit'
-  get 'goals/update'
-  get 'goals/destroy'
-  devise_for :users
   root to: "pages#home"
+  devise_for :users
+  resources :goals, only: [:show, :new, :create, :edit, :update, :destroy] do
+    resources :tasks, only: [:new, :create, :edit, :update, :destory]
+  end
+  
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+#   get 'journals/index'
+#   get 'journals/show'
+#   get 'journals/new'
+#   get 'journals/create'
+#   get 'journals/edit'
+#   get 'journals/update'
+#   get 'journals/destroy'
 end
