6432cf4793294f330fb7e3ed043d75888b550958
Siya-max97 <labasesiyamthanda0@gmail.com>
2023-09-27 17:53:02 +0200

    create a subtask

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,33 @@
 class TasksController < ApplicationController
   def new
+    @goal = Goal.find(params[:goal_id])
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @goal = Goal.find(params[:goal_id])
+    @task.goal = @goal
+    if @task.save
+      redirect_to goal_path(@goal), notice: 'Task was successfully created.'
+    else
+      render :new, alert: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
+  #   def edit
+  #     @task = Task.find(params[:id])
+  #   end
 
-  def update
-  end
+  #   def update
+  #   end
 
-  def destroy
+  #   def destroy
+  #   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
-  
 end
