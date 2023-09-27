427e64693ba9b612e5f9f3b9448bbec1b41817f9
Jenovi Wayela <wjenovi@gmail.com>
2023-09-27 17:52:01 +0200

    Phase 1 api implementation

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,45 @@
 class Goal < ApplicationRecord
   belongs_to :user
   has_many :tasks
+
+  # def submit_prompt
+  #   prompt = params[:prompt]
+
+  #   puts "Prompt: #{prompt}" # Debugging line
+  #   response = OpenaiService.new("#{prompt} for 2 days").call
+  #   puts "Response: #{response}"
+
+  #   days = (3)
+
+  #   tasks = []
+  #   @goal = Goal.find(params[:id])
+  #     @task = Task.new(description: response) #, goal_id: @goal.id)
+  #     @task.goal = @goal
+  #     @task.save
+
+  #   render :home
+  # end
+
+  def submit_prompt
+    # Get the user's goal
+    @goal = current_user.goals.find(params[:id])
+    @goal.submit_prompt
+
+    # Generate a task description based on the user's goal
+    task_description = generate_task_description(@goal)
+
+    # Use the OpenaiService to generate a response
+    response = OpenaiService.new(task_description).call
+
+    # Create a new task associated with the goal
+    @task = @goal.tasks.build(description: response)
+
+    if @task.save
+      redirect_to @goal, notice: 'New task generated successfully!'
+    else
+      flash.now[:alert] = 'Error generating the task.'
+      render :new
+    end
+  end
+
 end
