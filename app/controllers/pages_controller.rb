eb4da5a6ea162d791fe8884feebc7fb693062814
Jenovi Wayela <wjenovi@gmail.com>
2023-09-27 11:13:12 +0200

    prompt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,8 +10,21 @@ class PagesController < ApplicationController
 
   def submit_prompt
     prompt = params[:prompt]
-    @response = OpenaiService.new(prompt).call
-    puts @response
+    end_date = Date.parse(params[:end_date]) # Parse the end date from the form
+
+    # Calculate the number of days between today and the end date
+    days = (end_date - Date.today).to_i
+
+    # Initialize an array to store tasks for each day
+    tasks = []
+
+    # Loop through each day and generate tasks
+    (0..days).each do |day|
+      response = OpenaiService.new("#{prompt} for day #{day + 1}").call
+      tasks << response
+    end
+
+    @tasks = tasks
     render "pages/home"
   end
 end
