d5ffd31cf71c62bad0a80817d25de576ed9f1195
Carine Rugorirwera <carinerugorirwera@gmail.com>
2023-09-27 11:07:51 +0200

    Unested destroy action in in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
   resources :goals, only: [:show, :new, :create, :edit, :update, :destroy] do
-    resources :tasks, only: [:new, :create, :edit, :update, :destory]
+    resources :tasks, only: [:new, :create, :edit, :update]
   end
-  
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-  # Defines the root path route ("/")
-  # root "articles#index"
-#   get 'journals/index'
-#   get 'journals/show'
-#   get 'journals/new'
-#   get 'journals/create'
-#   get 'journals/edit'
-#   get 'journals/update'
-#   get 'journals/destroy'
+  resources :tasks, only: :destroy
 end
