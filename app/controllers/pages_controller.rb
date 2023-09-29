3c1fcc2347ed4fae4dc1643f15c1ce48e0bc3c62
Jenovi Wayela <wjenovi@gmail.com>
2023-09-29 10:16:24 +0200

    motivational quote feature (p1)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def home
     # @quotes = MotivationalQuote.all.shuffle.take(1)
-    @quotes = MotivationalQuote.all.map &:text
+    @quotes = MotivationalQuote.take(1).map &:text
   end
 
   # def random_quotes
