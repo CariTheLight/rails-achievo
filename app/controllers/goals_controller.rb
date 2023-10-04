1fb9f035fe46156b6749be9015b188540b078940
Jenovi Wayela <wjenovi@gmail.com>
2023-10-04 15:56:48 +0200

    finished journal feature

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -46,7 +46,6 @@ class GoalsController < ApplicationController
 
     #   # Create a new task with the generated description
     #   @task = @goal.tasks.build(description: task_description)
-  end
 
   def generate_task_description(goal)
     "My goal is to #{goal.description}.
